9226e41de8d9062ea02b963cd0e17ea790d07ecd
s57589 <s57589@beuth-hochschule.de>
2015-04-28 18:27:44 +0200

    added package Aufgabe1

---
+++ src/Aufgabe1/ShowImage.java
@@ -0,0 +1,12 @@
+package Aufgabe1;
+
+import javax.swing.*;
+
+/**
+ * Created by Jonathan on 28.04.15.
+ */
+public class ShowImage {
+    private ShowImage() {
+        final JFileChooser chooseFile = new JFileChooser();
+    }
+}
