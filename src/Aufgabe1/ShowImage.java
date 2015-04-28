31667ea1b6a4f725cae5f8ecf7ddb8bdd1b0f87d
s57589 <s57589@beuth-hochschule.de>
2015-04-28 21:59:22 +0200

    ShowImage finished
    ToDo:
    - need to add comments

--- src/Aufgabe1/ShowImage.java
+++ src/Aufgabe1/ShowImage.java
@@ -1,12 +1,47 @@
 package Aufgabe1;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 /**
  * Created by Jonathan on 28.04.15.
+ * This class opens a dialog to choose a image, which get display afterwards
  */
-public class ShowImage {
-    private ShowImage() {
-        final JFileChooser chooseFile = new JFileChooser();
+
+
+public class ShowImage extends Panel {
+
+    BufferedImage image;
+
+    public ShowImage() {
+        try {
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+            int result = fileChooser.showOpenDialog(null);
+
+            if (result == JFileChooser.APPROVE_OPTION) {
+                File file = fileChooser.getSelectedFile();
+                image = ImageIO.read(file);
+                this.setSize(image.getWidth(), image.getHeight());
+            }
+        } catch (IOException ie) {
+            System.out.println("Error:"+ie.getMessage());
+        }
+    }
+
+    public void paint(Graphics g) {
+        g.drawImage( image, 0, 0, null);
+    }
+
+    public static void main(String args[]) throws Exception {
+        JFrame frame = new JFrame("Display image");
+        Panel panel = new ShowImage();
+        frame.getContentPane().add(panel);
+        frame.setSize(panel.getWidth(), panel.getHeight());
+        frame.setResizable(false);
+        frame.setVisible(true);
     }
 }
