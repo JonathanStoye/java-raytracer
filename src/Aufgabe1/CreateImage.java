e0f4d48b40c0d2d4a6cd2408ac7e5f39e8371104
David Derichs <s57343@beuth-hochschule.de>
2015-04-30 23:17:41 +0200

    Klasse CreateImage stellt jetzt die Diagonale richtig dar. Ein Component-Listener zeichnet das Bild neu, sobald sich die Größe des beinhaltenden Frames ändert. Im Folgenden wird die Klasse Refactored. Methoden werden atomarer und die Variablen erhalten sprechendere Namen.

--- src/Aufgabe1/CreateImage.java
+++ src/Aufgabe1/CreateImage.java
@@ -2,6 +2,7 @@ package Aufgabe1;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 import java.awt.image.BufferedImage;
 
 /**
@@ -9,10 +10,22 @@ import java.awt.image.BufferedImage;
  *
  * Created by David on 28.04.2015.
  */
-public class CreateImage extends JFrame {
+public class CreateImage extends JFrame{
 
     BufferedImage buff;
     JPanel panel;
+
+    public CreateImage(){
+        System.out.println("neues Objekt vom Typ \"createImage\" wurde erzeugt");
+        this.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                super.componentResized(e);
+                System.out.println("Das Fenster wird groesser.");
+                createKoordinatensystem(e.getComponent().getWidth(), e.getComponent().getHeight());
+            }
+        });
+    }
 
     /**
      * The private class MyJPanel is used to override the .paint(Graphics g) Method in order to ensure
@@ -25,44 +38,50 @@ public class CreateImage extends JFrame {
         }
     }
 
-    public CreateImage(){
-        System.out.println("neues Objekt vom Typ \"createImage\" wurde erzeugt");
-    }
-
     /**
      * The Method createKoordinatensystem builds a panel in the given size.
      * Then a BufferedImage is used in order to Draw Pixels in the Area.
      * */
-    public void createKoordinatensystem(int hoehe, int breite){
+    public void createKoordinatensystem(int breite, int hoehe){
 
         /**
          * A new Panel is created, which inherits all the Elements shown in the Application.
          * */
         System.out.println("Erzeugen des Panels mit einer Hoehe von: "+hoehe+" und einer Breite von: "+breite);
         panel = new MyJPanel();
-        add(panel);
-        panel.setPreferredSize(new Dimension(hoehe, breite));
-        setSize(hoehe, breite);
+        panel.setPreferredSize(new Dimension(breite, hoehe));
+        panel.setLocation(50, 50);
+        this.setSize(new Dimension(breite, hoehe));
+        this.add(panel);
 
         /**
-         * The BufferedImage buff is used to create a Graphics Element and to setup the RGB Colors.
+         * The BufferedImage buff is used to set the pixels based on RGB values.
          * */
         System.out.println("Neues Image wird erzeugt.");
-        buff = new BufferedImage(hoehe, breite, BufferedImage.TYPE_INT_RGB);
-        int rgb = buff.getRGB(1,1);
+        buff = new BufferedImage(breite, hoehe, BufferedImage.TYPE_INT_RGB);
+        int rgb = 0xFFFF0000;
         int width = buff.getWidth();
         int height = buff.getHeight();
+        int[] rgbValues = new int[width * height];
 
-        buff.setRGB(1,1,rgb);
+        buff.getRGB(0,0,width,height,rgbValues,0,width);
 
-        Image g = buff.getScaledInstance(hoehe, breite, 1);
-
-
-
+        /**
+         * The Image is now filled with Pixels drawing a diagonal line.
+         * */
+        if(width>height) {
+            for (int i = 0; i < height; i++) {
+                buff.setRGB(i, i, rgb);
+            }
+        } else {
+            for (int i = 0; i < width; i++) {
+                buff.setRGB(i, i, rgb);
+            }
+        }
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
-
+        setBackground(Color.BLACK);
     }
 
 }
