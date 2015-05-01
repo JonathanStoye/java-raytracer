a10fd7453de9ec51ec6cfa52a4217522cebc5328
David Derichs <s57343@beuth-hochschule.de>
2015-05-01 13:04:38 +0200

    Habe die Klassen-Variablen bzw. -Methoden auskommentert, da sonst die Main einen Fehler wirft.

--- src/MatrixVector/Mat3x3.java
+++ src/MatrixVector/Mat3x3.java
@@ -14,43 +14,55 @@ public class Mat3x3 {
 //    +m32 : double
 //    +m33 : double
 // +determinant : double
-    Double m11;
-    Double m12;
-    Double m13;
-    Double m21;
-    Double m22;
-    Double m23;
-    Double m31;
-    Double m32;
-    Double m33;
+
+    /**
+     * Zu Testzwecken auskommentiert:
+     * */
+//    Double m11;
+//    Double m12;
+//    Double m13;
+//    Double m21;
+//    Double m22;
+//    Double m23;
+//    Double m31;
+//    Double m32;
+//    Double m33;
 
 //    +mul(eing. m : Mat3x3) : Mat3x3
 //    +mul(eing. v : Vector3) : Vector3
 //    +mul(eing. p : Point3) : Point3
-    public Mat3x3 mul(Mat3x3 m) {
 
-    }
-
-    public Vector3 mul(Vector3 v) {
-
-    }
-
-    public Point3 mul(Point3 p) {
-
-    }
+    /**
+     * Zu Testzwecken auskommentiert:
+     * */
+//    public Mat3x3 mul(Mat3x3 m) {
+//
+//    }
+//
+//    public Vector3 mul(Vector3 v) {
+//
+//    }
+//
+//    public Point3 mul(Point3 p) {
+//
+//    }
 
 //    +changeCol1(eing. v : Vector3) : Mat3x3
 //    +changeCol2(eing. v : Vector3) : Mat3x3
 //    +changeCol3(eing. v : Vector3) : Mat3x3
-    public Mat3x3 changeCol1(Vector3 v) {
 
-    }
-
-    public Mat3x3 changeCol2(Vector3 v) {
-
-    }
-
-    public Mat3x3 changeCol3(Vector3 v) {
-
-    }
+    /**
+     * Zu Testzwecken auskommentiert:
+     * */
+//    public Mat3x3 changeCol1(Vector3 v) {
+//
+//    }
+//
+//    public Mat3x3 changeCol2(Vector3 v) {
+//
+//    }
+//
+//    public Mat3x3 changeCol3(Vector3 v) {
+//
+//    }
 }
