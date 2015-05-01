dacb39a81ac1feccc6ad4dbf46ec426d1dfa0431
s57589 <s57589@beuth-hochschule.de>
2015-05-01 12:22:38 +0200

    Added the MatrixVector Package classes: - Mat3x3 (Jonathan) - Normal3 - Point3 - Vector3

--- src/MatrixVector/Mat3x3.java
+++ src/MatrixVector/Mat3x3.java
@@ -4,4 +4,53 @@ package MatrixVector;
  * Created by Jonathan on 01.05.15.
  */
 public class Mat3x3 {
+//    +m11 : double
+//    +m12 : double
+//    +m13 : double
+//    +m21 : double
+//    +m22 : double
+//    +m23 : double
+//    +m31 : double
+//    +m32 : double
+//    +m33 : double
+// +determinant : double
+    Double m11;
+    Double m12;
+    Double m13;
+    Double m21;
+    Double m22;
+    Double m23;
+    Double m31;
+    Double m32;
+    Double m33;
+
+//    +mul(eing. m : Mat3x3) : Mat3x3
+//    +mul(eing. v : Vector3) : Vector3
+//    +mul(eing. p : Point3) : Point3
+    public Mat3x3 mul(Mat3x3 m) {
+
+    }
+
+    public Vector3 mul(Vector3 v) {
+
+    }
+
+    public Point3 mul(Point3 p) {
+
+    }
+
+//    +changeCol1(eing. v : Vector3) : Mat3x3
+//    +changeCol2(eing. v : Vector3) : Mat3x3
+//    +changeCol3(eing. v : Vector3) : Mat3x3
+    public Mat3x3 changeCol1(Vector3 v) {
+
+    }
+
+    public Mat3x3 changeCol2(Vector3 v) {
+
+    }
+
+    public Mat3x3 changeCol3(Vector3 v) {
+
+    }
 }
