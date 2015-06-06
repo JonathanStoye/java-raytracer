9779bd523e1758962d3d55cfbdf06aab0cd835f7
David Derichs <s57343@beuth-hochschule.de>
2015-06-06 13:44:28 +0200

    Class Geometry implemented.

--- src/Geometry/Geometry.java
+++ src/Geometry/Geometry.java
@@ -4,10 +4,26 @@ import Scene.*;
 
 /**
  * Created by Jonathan on 05.06.15.
+ * Implemented by David on 06.06.15.
+ * Abstract Class, used to describe geometric forms in a scene.
  */
-public class Geometry {
+public abstract class Geometry {
+    /**
+     * Color of the form
+     */
+    public final Color color;
 
-    public Hit hit(Ray ray) {
-        return null;
+    /**
+     * Initializes the geometric form with the given parameter.
+     * @param inputColor Specifies the color of a certain geometric form
+     */
+    Geometry (Color inputColor){
+        this.color = inputColor;
     }
+
+    /**
+     * Determines wether the given ray is hitting the current object.
+     * @param ray specific ray, which is potentially hitting the current object.
+     */
+    public abstract Hit hit(final Ray ray);
 }
