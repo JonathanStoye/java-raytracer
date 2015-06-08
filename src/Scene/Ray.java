89c4fa76c9369ce31aa1a5ade26edfe7f8496113
David Derichs <s57343@beuth-hochschule.de>
2015-06-08 21:09:55 +0200

    Changed class attributes origin and direction to final.

--- src/Scene/Ray.java
+++ src/Scene/Ray.java
@@ -7,8 +7,8 @@ import MatrixVector.Vector3;
  * Created by Jonathan on 05.06.15.
  */
 public class Ray {
-    public static Point3 origin;
-    public static Vector3 direction;
+    public final Point3 origin;
+    public final Vector3 direction;
 
     public Ray(Point3 origin, Vector3 direction) {
         this.origin = origin;
