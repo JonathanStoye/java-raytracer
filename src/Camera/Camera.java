f126be00b4136572b4fa7cc34cf160abd0c69dac
David Derichs <s57343@beuth-hochschule.de>
2015-06-08 21:21:34 +0200

    Implemented Class camera. David

--- src/Camera/Camera.java
+++ src/Camera/Camera.java
@@ -5,24 +5,62 @@ import MatrixVector.Vector3;
 import Scene.Ray;
 
 /**
+ * Class Camera is used to to describe the basic parameters of a Camera in the szene
  * Created by Jonathan on 05.06.15.
+ * Implemented by David on 08.06.15.
+ * @author d.derichs
  */
-public class Camera {
+public abstract class Camera {
+
+    public final Point3 e;
+    public final Vector3 g;
+    public final Vector3 t;
+    public final Vector3 u;
+    public final Vector3 v;
+    public final Vector3 w;
 
     /**
-     * Constructor
+     * Initiates the Camera using the three vector parameters.
+     * Also calculates a new coordinate system based on this information.
+     * @param e point of view - vector of the camera
+     * @param g viewers direction - vector of the camera
+     * @param t up-vector of the camera
      */
+    public Camera(Point3 e, Vector3 g, Vector3 t) {
+        this.e = e;
+        this.g = g;
+        this.t = t;
 
-    Point3 e = this.e;
-    Vector3 g = this.g;
-    Vector3 t = this.t;
-    double s = this.s;
+        // Now Calculating vector w
+        // The formular for this vector looks like this
+        //          g
+        //  w = - -----
+        //         |g|
+        this.w = g.mul(1.0/g.magnitude).mul(-1.0);
 
-    public Camera(Point3 e, Vector3 g, Vector3 t, double s) {
+        // Now calculationg vector u
+        // The formuar for this vector looks like this
+        //      t x w
+        // u = ---------
+        //      |t x w|
+        this.u = t.x(w).mul(1.0/t.x(w).magnitude);
 
+        // Now calculating vector v
+        // The formular for this vector looks like this
+        //
+        // v = w x u
+        //
+        this.v = w.x(u);
     }
 
-    public Ray rayfor(int w, int h, int x, int y) {
-        return null;
-    }
+    /**
+     * rayFor returns a ray, which is calculated using the given parameters
+     * Based on how a camera works. All ray's vector's origins are based on the camera's values.
+     * @param w Width value of the picture
+     * @param h Height value of the picture
+     * @param x x-coordinate value of a pixel, for which the ray is calculated
+     * @param y y-coordinate value of a pixel, for which the ray is calculated
+     * @return Calculated Ray based on a certain pixel
+     */
+    public abstract Ray rayFor(int w, int h, int x, int y);
 }
