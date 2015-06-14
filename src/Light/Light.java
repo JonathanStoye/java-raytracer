47d9eefc25a83a2c01209500d75648241935958e
David Derichs <s57343@beuth-hochschule.de>
2015-06-14 17:48:50 +0200

    Implemented class Light.

--- src/Light/Light.java
+++ src/Light/Light.java
@@ -1,7 +1,40 @@
 package Light;
 
+import MatrixVector.Point3;
+import MatrixVector.Vector3;
+
+import java.awt.*;
+
 /**
- * Created by Jonathan on 09.06.15.
+ * Created by David on 14.06.15.
+ * Class Light is used to describe the basic parameters that light has in this Raytracer
+ * @author d.derichs
  */
-public class Light {
+public abstract class Light {
+
+    // Color of the Light
+    final public Color color;
+
+    /**
+     * Initiates the Light-Object using the given Color
+     * @param color Color of the Light
+     */
+    public Light(Color color){
+        this.color=color;
+    }
+
+    /**
+     * Determines wheter the given Point is illuminated by this light.
+     * @param point Point, which is potentially illuminated by this light.
+     * @return Returns a boolean value: true, if the Point is illuminated. false if the Point is not illuminated.
+     */
+    public abstract boolean illuminates(Point3 point);
+
+    /**
+     * Returns a Vector, representing the Direction from the given Point to the source of light.
+     * @param point Point, which is the Origin of the Vector that is pointing to the source of light.
+     * @return A Vector pointing to the source of light.
+     */
+    public abstract Vector3 directionFrom(Point3 point);
+
 }
