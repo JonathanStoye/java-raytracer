59ec8c2dc8a170adb0c4df1b538da701bd3aa9da
s57589 <s57589@beuth-hochschule.de>
2015-06-15 11:49:59 +0200

    implemention started

--- src/Material/LambertMaterial.java
+++ src/Material/LambertMaterial.java
@@ -1,7 +1,37 @@
 package Material;
 
+import Scene.Hit;
+import Scene.World;
+import Scene.Color;
+
 /**
  * Created by Jonathan on 09.06.15.
+ *
+ * LambertMaterial is the Material for a perfect diffuse reflecting material.
  */
-public class LambertMaterial {
+public class LambertMaterial extends Material {
+
+    public final Color color;
+
+    public LambertMaterial(Color color) {
+        this.color = color;
+    }
+
+    /**
+     * calculates the color for given hit with
+     *
+     * (formula)
+     *
+     * @param hit Hit object which is analysed
+     * @param world World object which is used to get information about the light in the current scene.
+     * @return the calculated Colr
+     */
+    @Override
+    public Color colorFor(Hit hit, World world) {
+        Color returnColor;
+
+        returnColor = this.color.mul(world.ambientLight);
+
+        return returnColor;
+    }
 }
