761f171dad9380e9775858498280cca55806e5b2
David Derichs <s57343@beuth-hochschule.de>
2015-06-15 11:16:44 +0200

    Implemented SimpleColorMaterial.

--- src/Material/SingleColorMaterial.java
+++ src/Material/SingleColorMaterial.java
@@ -1,7 +1,33 @@
 package Material;
 
+import Scene.Hit;
+import Scene.World;
+
+import java.awt.*;
+
 /**
- * Created by Jonathan on 09.06.15.
+ * Class SingleColorMaterial is used to describe the simplest Material, which only has a Color.
+ * Created by David on 15.06.15.
  */
-public class SingleColorMaterial {
+public class SingleColorMaterial extends Material{
+    final public Color color;
+
+    /**
+     * Initiates the Material using the given Color.
+     * @param color
+     */
+    public SingleColorMaterial(Color color){
+        this.color=color;
+    }
+
+    /**
+     * Just returns the Color of the Material
+     * @param hit Hit object which is analysed
+     * @param world World objekct which is used to get information about the light in the current scene.
+     * @return Color of the Material
+     */
+    @Override
+    public Color colorFor(Hit hit, World world) {
+        return this.color;
+    }
 }
