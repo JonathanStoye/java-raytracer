d2673d9a27573da3f9565d300f5b844bf02a76a1
David Derichs <s57343@beuth-hochschule.de>
2015-06-15 18:16:33 +0200

    Test SpotLight with PhongLight ist working

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
         //testAll();
 //        Color.test();
         Raytracer raytracer = new Raytracer(800, 600);
-        raytracer.testPhongLightning();
+        raytracer.testPhongLightningSpotLight();
     }
 
     private static void testAll() {
