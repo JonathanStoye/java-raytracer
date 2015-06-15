a89b33b21c4e30ef2f09128da01236ec9a55533c
s57589 <s57589@beuth-hochschule.de>
2015-06-15 11:47:11 +0200

    finished SingleColorMaterial (comments missing)

--- src/Material/SingleColorMaterial.java
+++ src/Material/SingleColorMaterial.java
@@ -1,7 +1,26 @@
 package Material;
 
+import Scene.*;
+
 /**
  * Created by Jonathan on 09.06.15.
  */
-public class SingleColorMaterial {
+public class SingleColorMaterial extends Material {
+
+    public final Color color;
+
+    public SingleColorMaterial(Color color) {
+        this.color = color;
+    }
+
+    /**
+     * simply return this.color for every hit
+     * @param hit Hit object which is analysed
+     * @param world World object which is used to get information about the light in the current scene.
+     * @return this.color
+     */
+    @Override
+    public Scene.Color colorFor(Hit hit, World world) {
+        return this.color;
+    }
 }
